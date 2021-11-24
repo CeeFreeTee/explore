bc79a4dc117b98af6c97bdd5db3c0bdc3fde428d
Tyson <c0023814@hallam.shu.ac.uk>
2021-11-24 00:02:52 +0000

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I'm making this by following the Gatsby Tutorial.</p>#
+      <StaticImage 
+        alt="loveit"
+        src="https://i.ytimg.com/vi/xT04nnT17NA/hqdefault.jpg"
+      />
     </Layout>
   )
 }
